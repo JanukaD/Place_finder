2f19377e312c4a91dc81fbac19d00e94d552b6c6
JanukaD <janukamadushan1995@gmail.com>
2019-09-12 10:48:15 +0530

    Create page structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import "./App.css";
 
+class App extends Component {
+  render() {
+    return (
 
-function App() {
-  return (
-    <div className="App">
-    <h1>search</h1>
-    </div>
-  );
+      <div className='container'>
+        <div className="row">
+          <div className="col-md-8">
+            <form>
+              <fieldset>
+                <legend>Search Nearby Places</legend>
+
+                <div className="col-md-10">
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Place:</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search here" />
+                  </div>
+                </div>
+
+                <div className="col-md-2">
+                  <button className="btn btn-success">Search</button>
+                </div>
+              </fieldset>
+            </form>
+            <ul>
+              <li>colombo</li>
+              <li>Kandy</li>
+            </ul>
+
+          </div>
+
+          <div className="col-md-4">
+            <h2>Save Places</h2>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
